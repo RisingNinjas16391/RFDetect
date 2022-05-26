f02c94f28a5a441e06cc56df81b0085a685d1984
RisingNinjas16391 <57425816+RisingNinjas16391@users.noreply.github.com>
2022-05-26 17:56:44 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,11 +1,11 @@
-var log = document.getElementById("log");
-
-            setInterval(() => {
-                fetch('logpage')
-                .then(response => {
-                        response.text().then(t => {log.innerHTML = t})
-                    });
-                }, 1000);
+//var log = document.getElementById("log");
+//
+//            setInterval(() => {
+//               fetch('logpage')
+//                .then(response => {
+//                        response.text().then(t => {log.innerHTML = t})
+//                    });
+//                }, 1000);
 
 $(function() {
     $('a#center').on('click', function(e) {
