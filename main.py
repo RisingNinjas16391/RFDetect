b7c4e581d74e1d8fe5c04d4a6af477fa45ebbcc9
RisingNinjas16391 <57425816+RisingNinjas16391@users.noreply.github.com>
2022-05-26 17:57:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from flask import Flask, escape
 from flask import render_template, request, Response
 
 import RPi.GPIO as GPIO
-
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/centeragitator')
+def centera():
+    //need to create function
+    return "oops"
   
 if __name__ == "__main__":
    print("Start")
